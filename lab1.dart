83b9118e51bfaa2540064b0dc2466b8b86dda336
yuravalchun <62293931+yuravalchun@users.noreply.github.com>
2020-09-16 15:48:13 +0300

    Update lab1.dart

--- lab1.dart
+++ lab1.dart
@@ -3,8 +3,10 @@ import 'dart:math';
 void main() {
     Rectangle rectangle = Rectangle(20, 30, 2);
     Circle circle = Circle(20, 4);
+    Circle ring = Circle.ring(20, 19, 4);
     print("Price for rectangle - " + rectangle.get_price());
     print("Price for circle - " + circle.get_price());
+    print("Price for ring - " + ring.get_price());
 }
 
 abstract class Figure {
@@ -40,12 +42,18 @@ class Rectangle extends Figure {
 
 class Circle extends Figure {
     double radius;
+    double small_radius = 0;
   
     Circle(radius, price_per_square_meter) : super(price_per_square_meter) {
       this.radius = radius;
     }
+  
+    Circle.ring(radius, small_radius, price_per_square_meter) : super(price_per_square_meter) {
+      this.radius = radius;
+      this.small_radius = small_radius;
+    }
       
     void calculate_area(){
-        this.area = pi * pow(this.radius, 2);
+        this.area = pi * (pow(this.radius, 2) - pow(this.small_radius, 2));
     }
 }
